0a505b1eff6bd672b1ad2d8bd63f5d1d8e758755
af <andy@fefelovgroup.com>
2019-01-30 04:43:04 +0500

    featuree (wal-g): backup/archive scripts

--- archive_command.sh
+++ archive_command.sh
@@ -2,6 +2,5 @@
 set -e
 source /etc/container_environment.sh
 if [ ! -z "$WALE_S3_PREFIX" ]; then
-    wal-g wal-push /var/lib/postgresql/data/$1
- 2>&1 | /usr/bin/logger -t wal-g
+    wal-g wal-push /var/lib/postgresql/data/$1  > /dev/null 2>&1
 fi
--- restore_command.sh
+++ restore_command.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-source /etc/container_environment.sh; wal-g wal-fetch $1 $2
+source /etc/container_environment.sh; wal-g wal-fetch $1 $2  > /dev/null 2>&1
